27f5eaaaacfeb14e31105dd891565330f9306464
Grace Burns <graceburns@chicagobooth.edu>
2023-04-19 14:48:14 +0000

    progress

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,24 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+
+    render({ :template => "directors_template/index.html.erb"})
+
+  end 
+
+  def wisest
+    @eldest = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+
+    render({ :template => "directors_template/eldest.html.erb"})
+  end
+
+  def director_details
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({:id => the_id}).at(0)
+
+    render({ :template => "directors_template/details.html.erb"})
+  end
+end
